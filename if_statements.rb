2a15f9728d944cf9b10177e1b11774653b5635f7
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-16 17:18:05 -0600

    Add if statement answers

--- if_statements.rb
+++ if_statements.rb
@@ -10,7 +10,7 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-weather = "sunny"
+weather = "icy"
 
 if weather == "sunny"
   p "sunscreen"
@@ -41,10 +41,13 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 2
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+if num_quarters == 2
+  p "I have enough money for a gumball"
+else
+  p "I don't have enough money for a gumball"
+end
 
 
 #####################
@@ -61,5 +64,11 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
-has_sauce = true
+cups_of_flour = 3
+has_sauce = false
+
+if cups_of_flour < 2 || has_sauce == false
+  p "I cannot make pizza"
+else cups_of_flour > 2 && has_sauce == true 
+  p "I can make pizza"
+end
