58992151b663b92ea80cdab9a3ba7a5d284aec49
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-23 17:21:38 -0600

    Add medium challenge

--- code_challenges/driving_record_challenge.rb
+++ code_challenges/driving_record_challenge.rb
@@ -1,1 +1,13 @@
 # Medium Challenge
+# Medium Challenge
+
+good_driving_record = false
+age = 21
+
+if good_driving_record == true && age > 25
+    puts "You earned a discount!"
+elsif good_driving_record == true || age > 25
+    puts "You pay full price."
+else good_driving_record == false && age < 25
+    puts "You need a cosigner."
+end
