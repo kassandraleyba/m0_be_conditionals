bb4533faa826dca5d04392759db602011f81a697
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-24 08:41:19 -0600

    Add corrections

--- code_challenges/driving_record_challenge.rb
+++ code_challenges/driving_record_challenge.rb
@@ -1,13 +1,12 @@
 # Medium Challenge
-# Medium Challenge
 
-good_driving_record = false
-age = 21
+good_driving_record = true
+age = 29
 
 if good_driving_record == true && age > 25
     puts "You earned a discount!"
 elsif good_driving_record == true || age > 25
     puts "You pay full price."
-else good_driving_record == false && age < 25
+else 
     puts "You need a cosigner."
 end
